7f0678d5f93ff0972d21f69248e8b5245d959a4f
david nyong <davydevelops@gmail.com>
2024-05-18 17:06:16 +0100

    initial commit

---
+++ data-types.js
@@ -0,0 +1,25 @@
+// there are _ data types in js
+
+//these are the forms of data in js
+
+/**
+ * 1. boolean
+ * 2. numbers
+ * 3. big int
+ * 4. strings
+ * 5. null
+ * 6. undefined
+ * 7. symbol
+ * 8. Object(obj, arrays, json, etc..)
+ */
+
+
+console.log("vicky"); //string
+console.log(30); // number
+console.log(null); //null
+
+let noAssignment;
+console.log(noAssignment); //unddefined
+
+// let symbI = $
+// console.log(symbI); //symbol
