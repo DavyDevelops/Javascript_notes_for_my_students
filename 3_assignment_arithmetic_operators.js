1f090abb0fca98fac2d11c4fa2fcccb1ed75f197
david nyong <davydevelops@gmail.com>
2024-05-18 17:30:25 +0100

    Arithmetic operators + Assignment Operators

---
+++ 3_assignment_arithmetic_operators.js
@@ -0,0 +1,39 @@
+//operators in Javascript
+
+//Arithmetic operators in js
+
+/**
+ * plus (+)
+ * minus(-)
+ * divide(/)
+ * power/exponents(**)
+ * modulus(%) remainder operator
+ */
+
+//examples of arithmetic operators
+
+// console.log(30+5);
+// console.log(30-5);
+// console.log(30/5);
+// console.log(3**5);
+// console.log(0%1);
+
+
+// Assignment operator
+
+let x = 30/5
+console.log(x) //x was 6
+//incremental operator
+x++
+console.log(x) //x became 7
+
+x+=10
+console.log(x) // x=x(7)+10 x became 17
+
+x-=10
+console.log(x) // x=x(7)-10 x became 7
+
+x-- // decrement by 1 x is 6
+console.log(x)
+x/=2
+console.log(x) // x=x(6)/2 x became 3
