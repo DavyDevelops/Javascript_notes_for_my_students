59acd7ab6d2dd0c406cf95fbb8b07f8b7ce68ab6
david nyong <davydevelops@gmail.com>
2024-05-18 18:10:29 +0100

    6 & 7 added

---
+++ 6_IF_else_statements.js
@@ -0,0 +1,14 @@
+// conditional statements, in taking decisions in JS conditional statements evaluate if a condition is true or false before carrying out
+
+const personAge = "goood";
+
+if(personAge >= 18){
+    console.log("congrats you are old enough to ride this roller coaster")
+} else if(personAge <= 18) {
+    console.log("Oh sorry you are not old enough to ride this roller coaster")
+} else {
+    console.log("thats not a number")
+}
+
+//Basically were saying if the variable declared isnt greater than pr equals to 18
+// Then run the other block of else code and if it is log something to the console
---
+++ 7_type-conversion-coercion.js
@@ -0,0 +1,10 @@
+/**
+ * Type conversion is when we convert a data type from one form to another
+ * 
+ * type conversion is when javascript forces a convertion from one form to another
+ * 
+ * typeof operator checks the type of a vaue
+ */
+
+
+//examples of type convertion 
